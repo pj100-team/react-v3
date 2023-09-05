46744ad9cc542fa80b5e62d5ff14e7a20a7c8d6d
kihara masaki <masaki_kihara@pj100.biz>
2023-09-05 16:34:53 +0900

    不要箇所削除

--- src/App.tsx
+++ src/App.tsx
@@ -1,44 +1,7 @@
 import "./App.css";
-import { Button } from "./components/button";
 
 function App() {
-	const button1Fn = () => {
-		console.log("button1");
-	};
-
-	const button2Fn = () => {
-		console.log("button2");
-	};
-
-	const button3Fn = () => {
-		console.log("button3");
-	};
-
-	return (
-		<div className="flex justify-center gap-10 pt-1">
-			<Button
-				buttonName={"button1"}
-				buttonStyle={
-					"border border-[#f87171] bg-[#f87171] w-[100px] text-[#f9fafb] rounded"
-				}
-				onClick={button1Fn}
-			/>
-			<Button
-				buttonName={"button2"}
-				buttonStyle={
-					"border border-[#fcd34d] bg-[#fcd34d] w-[100px] text-[#f9fafb] rounded"
-				}
-				onClick={button2Fn}
-			/>
-			<Button
-				buttonName={"button3"}
-				buttonStyle={
-					"border border-[#86efac] bg-[#86efac] w-[100px] text-[#f9fafb] rounded"
-				}
-				onClick={button3Fn}
-			/>
-		</div>
-	);
+	return <p className="text-center">チュートリアル</p>;
 }
 
 export default App;
