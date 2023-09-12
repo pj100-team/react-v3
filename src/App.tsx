4d53b4d5e0779d9799066584213ece4533ae7179
kihara masaki <masaki_kihara@pj100.biz>
2023-09-12 11:18:07 +0900

    👍ヘッダー修正

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,9 @@
 import "./App.css";
 
-export const App=()=> {
-	return <p className="text-center">チュートリアル</p>;
-}
-
+export const App = () => {
+	return (
+		<header className="flex justify-center bg-[#9ca3af] text-[#f9fafb] text-3xl p-[10px]">
+			React-v3
+		</header>
+	);
+};
