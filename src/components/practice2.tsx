93acb04505bfc2a1c9899c39385ec04a7b278e86
tomoya0527 <takahashi_tomoya@pj100.biz>
2024-01-15 14:36:07 +0900

    prettier設定完了

--- src/components/practice2.tsx
+++ src/components/practice2.tsx
@@ -1,14 +1,21 @@
 import Input from "./input";
-import { useState } from "react"
+import { useState } from "react";
 
-export default function Practice2(){
-	const [checkbox, setCheckbox] = useState<Boolean>(false)
-	const handleChange = () => {
-		setCheckbox(onChange => !onChange)
-	}
-	return (
-		<Input type="checkbox" name="greeting" style="max-w-fit mx-auto flex gap-4" onChange={handleChange} >
-			<p className={!checkbox ? 'opacity-0 invisible' : ''}>hello world!</p>
-		</Input>
-	)
+export default function Practice2() {
+    const [checkbox, setCheckbox] = useState<Boolean>(false);
+    const handleChange = () => {
+        setCheckbox((onChange) => !onChange);
+    };
+    return (
+        <Input
+            type="checkbox"
+            name="greeting"
+            style="max-w-fit mx-auto flex gap-4"
+            onChange={handleChange}
+        >
+            <p className={!checkbox ? "opacity-0 invisible" : ""}>
+                hello world!
+            </p>
+        </Input>
+    );
 }
--- src/templates/practice3.tsx
+++ src/templates/practice3.tsx
@@ -2,22 +2,37 @@ import Input from "../components/input";
 import Button from "../components/button";
 import { FormEvent, useState } from "react";
 
-const Practice3 : React.FC = () =>{
-	const [input, setInput] = useState<string>('')
-	const handleChange = (e : FormEvent<HTMLInputElement>) => {
-		setInput(e.currentTarget.value)
-	}
-	const handleClear = () => {
-		setInput('')
-	}
+const Practice3: React.FC = () => {
+    const [input, setInput] = useState<string>("");
+    const handleChange = (e: FormEvent<HTMLInputElement>) => {
+        setInput(e.currentTarget.value);
+    };
+    const handleClear = () => {
+        setInput("");
+    };
 
-	return (
-		<div className="flex flex-col items-center gap-5">
-			<Input type="text" name="word" style="p-form__input" onChange={handleChange} value={input} />
-			<Button type="button" onClick={handleClear} style={{txtColor:'text-[#f9fafb]',bgColor:'bg-[#f87171]',borderColor:'border-[#f87171]'}}>リセット</Button>
-		</div>
-	)
-}
+    return (
+        <div className="flex flex-col items-center gap-5">
+            <Input
+                type="text"
+                name="word"
+                style="p-form__input"
+                onChange={handleChange}
+                value={input}
+            />
+            <Button
+                type="button"
+                onClick={handleClear}
+                style={{
+                    txtColor: "text-[#f9fafb]",
+                    bgColor: "bg-[#f87171]",
+                    borderColor: "border-[#f87171]",
+                }}
+            >
+                リセット
+            </Button>
+        </div>
+    );
+};
 
-
-export default Practice3
+export default Practice3;
