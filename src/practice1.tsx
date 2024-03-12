9b1e6a04737531ff601162e28cc8de90d8869451
ShihminChou <shimin_zhou@pj100.biz>
2024-03-12 17:28:20 +0900

    [実装]Practice3

---
+++ src/practice1.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import Button from './components/button';
+
+const Practice1 = () => {
+  const getValue = (e: any) => {
+    console.log(e.target.value);
+  };
+  return (
+    <div className="flex justify-center py-5">
+      <Button value={'button1'} style="bg-[#f87171] text-[#f9fafb] rounded py-1 px-4 mx-2" onClick={getValue} />
+      <Button value={'button2'} style="bg-[#fcd34d] text-[#f9fafb] rounded py-1 px-4 mx-2" onClick={getValue} />
+      <Button value={'button3'} style="bg-[#86efac] text-[#f9fafb] rounded py-1 px-4 mx-2" onClick={getValue} />
+    </div>
+  );
+};
+
+export default Practice1;
---
+++ src/practice3.tsx
@@ -0,0 +1,23 @@
+import React, { useState } from 'react';
+import Input from './components/input';
+import Button from './components/button';
+
+const Practice3 = () => {
+  const [textValue, setTextValue] = useState<string>('');
+
+  const changeText = (e: any) => {
+    console.log(e.target.value);
+    setTextValue(e.target.value);
+  };
+  const resetText = () => {
+    setTextValue('');
+  };
+  return (
+    <div className="flex justify-center items-center">
+      <Input value={textValue} type="text" style="outline outline-1 outline-[#94a3b8] rounded" onChange={changeText} />
+      <Button value="Reset" style="bg-[#f87171] text-[#f9fafb] rounded py-1 px-4 mx-2" onClick={resetText} />
+    </div>
+  );
+};
+
+export default Practice3;
