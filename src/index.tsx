fa1d7518518cb73c3c41e10e734da5766e684528
keitaro2046 <keitaro_higuchi@pj100.biz>
2024-05-02 10:24:50 +0900

    practice1の修正

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import Practice1 from './practice1';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
   <React.StrictMode>
     <App />
+    <Practice1 />
   </React.StrictMode>,
 );
