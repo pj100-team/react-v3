efa14c0f9da5b1a4047b62662c7b8cd50e3bc486
keitaro2046 <keitaro_higuchi@pj100.biz>
2024-05-02 10:56:20 +0900

    practice1の修正

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import Practice1 from './practice1';
+import Practice1 from './templates/practice1';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
