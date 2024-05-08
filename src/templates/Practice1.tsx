0bad96c04c5f99d24aaa689d334de57df55b8fef
keitaro2046 <keitaro_higuchi@pj100.biz>
2024-05-08 13:14:29 +0900

    ファイル名差分検知

--- src/templates/Practice1.tsx
+++ src/templates/Practice1.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import Button from '../components/Button1';
+import Button from '../components/Button';
 
 const Practice1: React.FC = () => {
   const handleClick = (e: React.MouseEvent<HTMLButtonElement>) => {
@@ -9,6 +9,7 @@ const Practice1: React.FC = () => {
   return (
     <div className="flex justify-center mt-10 gap-5">
       <Button name="button1" color="border-[#f87171] bg-[#f87171]" onClick={handleClick}></Button>
+
       <Button name="button2" color="border-[#fcd34d] bg-[#fcd34d]" onClick={handleClick}></Button>
 
       <Button name="button3" color="border-[#86efac] bg-[#86efac]" onClick={handleClick}></Button>
