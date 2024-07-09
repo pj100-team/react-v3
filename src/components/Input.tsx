57ca78b4e30964af8ce9957e9c5de6697ae85615
keitaro2046 <keitaro_higuchi@pj100.biz>
2024-07-09 16:54:50 +0900

    childrenpropsプロップスの不要な消去の修正

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -7,12 +7,14 @@ type InputProps = {
   onChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
   onBlur?: () => void;
   value: string;
+  children?: React.ReactNode;
 };
 
-const Input: React.FC<InputProps> = ({ id, type, style, onChange, onBlur, value }) => {
+const Input: React.FC<InputProps> = ({ id, type, style, onChange, onBlur, value, children }) => {
   return (
     <>
       <label htmlFor={id}>
+        {children}
         <input id={id} type={type} style={style} onChange={onChange} onBlur={onBlur} value={value} />
       </label>
     </>
