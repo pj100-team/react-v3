5f81d3f9c22338e31e3b65b9686f16feea6d81c6
Tatsuya-Otsuki <tatsuya_otsuki@pj100.biz>
2024-09-03 10:06:59 +0900

    🚿 不要箇所の削除

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,9 @@
 import { useCallback, useState } from 'react';
 import './App.css';
-import CountText from './CountText';
 import Button from './Button';
 
 function App() {
   const [count, setCount] = useState<number>(0);
-  const [count2, setCount2] = useState<number>(0);
 
   // Callback
   const callback = useCallback(() => {
@@ -16,10 +14,8 @@ function App() {
     <>
       <div className="bg-gray-100">
         <p>Counter:App</p>
-        <CountText num={count} />
+        <p>count:{count}</p>
         <button onClick={() => setCount((count) => count + 1)}>add</button>
-        <p>count:{count2}</p>
-        <button onClick={() => setCount2((count2) => count2 + 1)}>add</button>
         <Button onClick={callback} />
       </div>
     </>
