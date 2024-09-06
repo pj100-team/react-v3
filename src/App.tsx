015134e7be41f617c32ccad044a6270fe065911d
Tatsuya-Otsuki <tatsuya_otsuki@pj100.biz>
2024-09-06 16:32:31 +0900

    👍 エラー文を追加

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,27 @@
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [error, setError] = useState<string>('');
+
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     const form = new FormData(e.currentTarget);
+
+    //formの入力値が空のバリデーションエラーを作成
+    if (!form.get('email')) {
+      console.log('email is required');
+      setError('email is required');
+      return;
+    }
+
+    if (!form.get('pass')) {
+      console.log('password is required');
+      setError('password is required');
+      return;
+    }
+
+    setError('');
     console.log(`email: ${form.get('email')}`);
     console.log(`password: ${form.get('pass')}`);
     console.log('form submitted');
@@ -16,6 +34,7 @@ function App() {
         <input name="pass" type="password" placeholder="Enter your password" />
         <button type="submit">Submit</button>
       </form>
+      <p className="text-red-400">{error}</p>
     </div>
   );
 }
