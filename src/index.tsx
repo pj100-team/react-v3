135780450fb32a1c2b9f1d266b6720803f22d815
bungai_ou <bungai_ou@pj100.biz>
2024-09-06 16:15:15 +0900

    👍　Copilot 高レベルテストの2

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import CopilotHighTest1 from './CopilotHighTest1';
+import CopilotHighTest2 from './CopilotHighTest2';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
   <React.StrictMode>
     {/* <App /> */}
-    <CopilotHighTest1 />
+    <CopilotHighTest2 />
   </React.StrictMode>,
 );
