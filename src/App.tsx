58fb9f2f9526544a1cdb59099813c87122fb5ec5
Tatsuya-Otsuki <tatsuya_otsuki@pj100.biz>
2024-09-09 13:37:04 +0900

    👍 デザインの追加

--- src/App.tsx
+++ src/App.tsx
@@ -57,18 +57,25 @@ function App() {
     getAddress(zipcode);
   }, [zipcode]);
 
+  // 以下の住所,エラー,読込中のtailwind classnameを作成
+  const addressClass = 'text-lg font-bold';
+  const errorClass = 'text-red-500';
+  const loadingClass = 'text-blue-500';
+  const inputClass = 'w-1/2 border-2 border-gray-300 rounded-md p-2';
+
   return (
     <div>
       <input
         type="text"
         name="zipcode"
+        className={inputClass}
         onChange={(e) => {
           changeValue(e);
         }}
       />
-      <p>address:{address}</p>
-      <p>error:{error}</p>
-      {isLoading && <p>loading...</p>}
+      <p className={addressClass}>address:{address}</p>
+      <p className={errorClass}>error:{error}</p>
+      {isLoading && <p className={loadingClass}>loading...</p>}
     </div>
   );
 }
