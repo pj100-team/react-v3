167c4454b38cdb5b65bca429e2fa19c26946a318
Tatsuya-Otsuki <tatsuya_otsuki@pj100.biz>
2024-09-09 11:45:31 +0900

    👍 context apiで値を受け渡し

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,69 @@
-import "./App.css";
+import { createContext, useContext, useState } from 'react';
+import './App.css';
+
+type UserType = {
+  name: string;
+  setName: React.Dispatch<React.SetStateAction<string>>;
+  email: string;
+  setEmail: React.Dispatch<React.SetStateAction<string>>;
+};
+const UserContext = createContext<UserType>({
+  name: '',
+  setName: () => {},
+  email: '',
+  setEmail: () => {},
+});
+
+const Component1 = () => {
+  const user = useContext(UserContext);
+  return (
+    <>
+      <h2>Component 1</h2>
+      <p>Name: {user.name}</p>
+      <p>Email: {user.email}</p>
+      <Component2 />
+    </>
+  );
+};
+
+const Component2 = () => {
+  const user = useContext(UserContext);
+  return (
+    <>
+      <h2>Component 2</h2>
+      <p>Name: {user.name}</p>
+      <p>Email: {user.email}</p>
+      <Component3 />
+    </>
+  );
+};
+
+const Component3 = () => {
+  const user = useContext(UserContext);
+  return (
+    <>
+      <h2>Component 3</h2>
+      <p>Name: {user.name}</p>
+      <p>Email: {user.email}</p>
+      <button onClick={() => user.setName('Jane Doe')}>Change Name</button>
+      <button onClick={() => user.setEmail('jane@example.com')}>Change Email</button>
+    </>
+  );
+};
 
 function App() {
-	return (
-		<header className="bg-[#94A3B8] text-center p-[20px] text-4xl text-[#F9FAFB]">
-			React-v3
-		</header>
-	);
+  const [name, setName] = useState<string>('John Doe');
+  const [email, setEmail] = useState<string>('john@example.com');
+  const user: UserType = { name, setName, email, setEmail };
+
+  return (
+    <>
+      <UserContext.Provider value={user}>
+        <h1>React Context API</h1>
+        <Component1 />
+      </UserContext.Provider>
+    </>
+  );
 }
 
 export default App;
