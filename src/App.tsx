309cf000678e54839111a04b9dcf8ff333e41ecc
Tatsuya-Otsuki <tatsuya_otsuki@pj100.biz>
2024-09-09 11:05:15 +0900

    👍 ログイン機能を作成

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,49 @@
-import "./App.css";
+import { useState } from 'react';
+import './App.css';
 
 function App() {
-	return (
-		<header className="bg-[#94A3B8] text-center p-[20px] text-4xl text-[#F9FAFB]">
-			React-v3
-		</header>
-	);
+  const [isLogin, setIsLogin] = useState<boolean>(false);
+  const [email, setEmail] = useState<string>('');
+  const [password, setPassword] = useState<string>('');
+
+  // ログインボタンのtailwind
+  const loginButton = 'bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded';
+
+  // ログイン状態のメッセージのtailwind
+  const loginMessage = 'text-blue-500';
+
+  // form提出時に、ダミーの値をemailとpasswordにセット
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    if (isLogin) {
+      setIsLogin(false);
+      setEmail('');
+      setPassword('');
+      return;
+    }
+
+    setIsLogin(true);
+    setEmail('hoge@example.com');
+    setPassword('password');
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <button className={loginButton} type="submit">
+        {isLogin ? 'ログアウト' : 'ログイン'}
+      </button>
+      <h1 className={loginMessage}>{isLogin ? 'ログイン中です' : 'ログインしてください'}</h1>
+      {isLogin && (
+        <div>
+          <p>メールアドレス</p>
+          <p>{email}</p>
+          <p>パスワード</p>
+          <p>{password}</p>
+        </div>
+      )}
+    </form>
+  );
 }
 
 export default App;
