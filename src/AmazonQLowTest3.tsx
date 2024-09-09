ca064e3bc0c2764703020c91a76e8b1fb6b3a7f3
bungai_ou <bungai_ou@pj100.biz>
2024-09-09 11:16:07 +0900

    👍 AmazonQ 低レベルテストの3

---
+++ src/AmazonQLowTest3.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+// Button component
+const AmazonQLowTest3 = () => {
+  const handleClick = () => {
+    alert('Button clicked!');
+  };
+
+  return <button onClick={handleClick}>Click me</button>;
+};
+
+export default AmazonQLowTest3;
