e84c0f2aeff816b6071af5d826b87efe13091314
Tatsuya-Otsuki <tatsuya_otsuki@pj100.biz>
2024-09-09 14:46:21 +0900

    👍 ボタンの追加

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { memo, useState } from 'react';
+import { memo, useCallback, useState } from 'react';
 import './App.css';
 
 const ShowCount = memo(({ cnt, no }: { cnt: number; no: number }) => {
@@ -6,24 +6,29 @@ const ShowCount = memo(({ cnt, no }: { cnt: number; no: number }) => {
   return <div>count:{cnt}</div>;
 });
 
+const Button = memo(({ onClick }: { onClick: () => void }) => {
+  console.log('button');
+  return <button onClick={onClick}>+</button>;
+});
+
 function App() {
   const [count1, setCount1] = useState<number>(0);
   const [count2, setCount2] = useState<number>(0);
 
-  const addCount1 = () => {
+  const addCount1 = useCallback(() => {
     setCount1(count1 + 1);
-  };
+  }, [count1]);
 
-  const addCount2 = () => {
+  const addCount2 = useCallback(() => {
     setCount2(count2 + 1);
-  };
+  }, [count2]);
 
   return (
     <>
       <ShowCount cnt={count1} no={1} />
-      <button onClick={addCount1}>+</button>
+      <Button onClick={addCount1} />
       <ShowCount cnt={count2} no={2} />
-      <button onClick={addCount2}>+</button>
+      <Button onClick={addCount2} />
     </>
   );
 }
