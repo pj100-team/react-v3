bc96933a4b62a3d147601128baf4a856a11744f4
Tatsuya-Otsuki <tatsuya_otsuki@pj100.biz>
2024-09-09 14:37:09 +0900

    ✨ カウンターの作成

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,31 @@
-import "./App.css";
+import { useState } from 'react';
+import './App.css';
+
+const ShowCount = ({ cnt, no }: { cnt: number; no: number }) => {
+  console.log(`show count:${no}`);
+  return <div>count:{cnt}</div>;
+};
 
 function App() {
-	return (
-		<header className="bg-[#94A3B8] text-center p-[20px] text-4xl text-[#F9FAFB]">
-			React-v3
-		</header>
-	);
+  const [count1, setCount1] = useState<number>(0);
+  const [count2, setCount2] = useState<number>(0);
+
+  const addCount1 = () => {
+    setCount1(count1 + 1);
+  };
+
+  const addCount2 = () => {
+    setCount2(count2 + 1);
+  };
+
+  return (
+    <>
+      <ShowCount cnt={count1} no={1} />
+      <button onClick={addCount1}>+</button>
+      <ShowCount cnt={count2} no={2} />
+      <button onClick={addCount2}>+</button>
+    </>
+  );
 }
 
 export default App;
