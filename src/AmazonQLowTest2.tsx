7aa568d4c3a7ee6eff918a18eb93c0c4bd544875
bungai_ou <bungai_ou@pj100.biz>
2024-09-09 11:03:04 +0900

    👍 AmazonQ 低レベルテストの2

---
+++ src/AmazonQLowTest2.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+// Sample data array
+const items = [
+  { id: 1, name: 'Item 1' },
+  { id: 2, name: 'Item 2' },
+  { id: 3, name: 'Item 3' },
+  { id: 4, name: 'Item 4' },
+];
+
+// List component
+const AmazonQLowTest2 = () => {
+  return (
+    <ul>
+      {items.map((item) => (
+        <li key={item.id}>{item.name}</li>
+      ))}
+    </ul>
+  );
+};
+
+export default AmazonQLowTest2;
