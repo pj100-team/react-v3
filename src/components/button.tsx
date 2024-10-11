0f3c113453f6687db2beb842d51cb85f28e95af9
SUKJIN <jin_soku@pj100.biz>
2024-10-11 15:35:15 +0900

    practice1課題修正提３

--- src/components/button.tsx
+++ src/components/button.tsx
@@ -1,27 +1,26 @@
 import React from 'react';
 
 interface ButtonProps {
-	label: string;
-	backgroundColor: string;
-	textColor: string;
-	handleClick: () => void;
-	className?: string;
+  label: string;
+  backgroundColor: string;
+  textColor: string;
+  className?: string;
 }
 
-const Button: React.FC<ButtonProps> = ({ label, backgroundColor, textColor, handleClick, className }) => {
-	return (
-		<button
-			className={`p-2 rounded border-2 ${className}`}
-			style={{
-				backgroundColor: backgroundColor,
-				color: textColor,
-				borderColor: backgroundColor,
-			}}
-			onClick={handleClick}
-		>
-			{label}
-		</button>
-	);
+const Button: React.FC<ButtonProps> = ({ label, backgroundColor, textColor, className = "" }) => {
+  const handleClick = () => {
+    console.log(label);
+  };
+
+  return (
+    <button
+      className={`p-2 rounded border-2 ${className}`}
+      style={{ backgroundColor, color: textColor, borderColor: backgroundColor }}
+      onClick={handleClick}
+    >
+      {label}
+    </button>
+  );
 };
 
 export default Button;
--- src/components/practice1.tsx
+++ src/components/practice1.tsx
@@ -2,37 +2,30 @@ import React from 'react';
 import Button from './button';
 
 const Practice1: React.FC = () => {
-	const handleClick = (buttonName: string) => {
-		console.log(buttonName);
-	};
-
-	return (
-		<div className="text-center rounded-2xl mx-auto mt-24">
-			<div className="flex justify-center mt-5 gap-10">
-				<Button
-					label="button1"
-					backgroundColor="#f87171"
-					textColor="#f9fafb"
-					className="px-11 py-4 text-lg"
-					handleClick={() => handleClick('button1')}
-				/>
-				<Button
-					label="button2"
-					backgroundColor="#fcd34d"
-					textColor="#f9fafb"
-					className="px-11 py-4 text-lg"
-					handleClick={() => handleClick('button2')}
-				/>
-				<Button
-					label="button3"
-					backgroundColor="#86efac"
-					textColor="#f9fafb"
-					className="px-11 py-4 text-lg"
-					handleClick={() => handleClick('button3')}
-				/>
-			</div>
-		</div>
-	);
+  return (
+    <div className="text-center rounded-2xl mx-auto mt-24">
+      <div className="flex justify-center mt-5 gap-10">
+        <Button
+          label="button1"
+          backgroundColor="#f87171"
+          textColor="#f9fafb"
+          className="px-11 py-4 text-lg"
+        />
+        <Button
+          label="button2"
+          backgroundColor="#fcd34d"
+          textColor="#f9fafb"
+          className="px-11 py-4 text-lg"
+        />
+        <Button
+          label="button3"
+          backgroundColor="#86efac"
+          textColor="#f9fafb"
+          className="px-11 py-4 text-lg"
+        />
+      </div>
+    </div>
+  );
 };
 
 export default Practice1;
