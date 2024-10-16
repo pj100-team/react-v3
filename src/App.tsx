17a1ba3c1b92e6e448c3ec0f62711964cf813f91
SUKJIN <jin_soku@pj100.biz>
2024-10-16 14:52:03 +0900

    practice2 課題提出３

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,6 @@
 import React from 'react';
 import './App.css';
-<<<<<<< HEAD
 import Practice2 from './components/practice2';
-=======
-import Practice1 from './components/practice1'; 
->>>>>>> 742c68d627595ae4624be6a3cf8d8d87f4f56cf0
 
 function App() {
 	return (
@@ -12,11 +8,7 @@ function App() {
 			<header className="bg-[#94A3B8] text-center p-[20px] text-4xl text-[#F9FAFB]">
 				React-v3
 			</header>
-<<<<<<< HEAD
 			<Practice2 />
-=======
-			<Practice1 />
->>>>>>> 742c68d627595ae4624be6a3cf8d8d87f4f56cf0
 		</div>
 	);
 }
