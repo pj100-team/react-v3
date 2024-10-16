9cf4a8ce403fd75ed33093efa38c6127c2f8b0e5
SUKJIN <jin_soku@pj100.biz>
2024-10-16 13:11:31 +0900

    practice2 課題提出

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import Practice1 from './components/practice1'; 
+import Practice2 from './components/practice2';
 
 function App() {
 	return (
@@ -8,7 +8,7 @@ function App() {
 			<header className="bg-[#94A3B8] text-center p-[20px] text-4xl text-[#F9FAFB]">
 				React-v3
 			</header>
-			<Practice1 />
+			<Practice2 />
 		</div>
 	);
 }
