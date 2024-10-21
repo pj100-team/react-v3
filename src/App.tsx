afc29c367b8f3afe3ddb3bf924dc39a01e9d0c77
SUKJIN <jin_soku@pj100.biz>
2024-10-21 16:43:36 +0900

    Resolve conflicts in src/App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import './App.css';
 import Input from './components/input';
+import Practice2 from './components/practice2';
 
 function App() {
 	return (
@@ -8,7 +9,8 @@ function App() {
 			<header className="bg-[#94A3B8] text-center p-[20px] text-4xl text-[#F9FAFB]">
 				React-v3
 			</header>
-			<input />
+			<Input />
+			<Practice2 />
 		</div>
 	);
 }
