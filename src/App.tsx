28a68e3f09b5eca64f2359c319cbb69de85faf92
SUKJIN <jin_soku@pj100.biz>
2024-10-22 16:34:13 +0900

    practice3 課題提出

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import Input from './components/input';
 import Button from './components/button';
 
 function App() {
+	const [intputText,setIntputText]=useState<string>("")
   return (
     <div>
       <header className="bg-[#94A3B8] text-center p-5 text-4xl text-white">
@@ -11,18 +12,20 @@ function App() {
       </header>
       <div className="flex justify-center my-10">
         <Input
+		 value={intputText}
           type="text"
-          onChange={(e) => {
-            console.log(e.target.value);
+          onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+		  setIntputText(e.target.value)
+		  console.log(e.target.value);
           }}
           className="h-8 border-2"
         />
         <Button
           buttonName="Reset"
           backGroundColor="red"
-          onClick={() => {
-            console.log("Reset button clicked");
-          }}
+          onClick={() => {setIntputText("")
+        }
+		}
         />
       </div>
     </div>
