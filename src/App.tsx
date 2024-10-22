20f6840a7be4544b193348bc4ba08d8473f5e921
SUKJIN <jin_soku@pj100.biz>
2024-10-22 15:14:47 +0900

    practice3課題提出

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,32 @@
 import React from 'react';
 import './App.css';
-import Practice2 from './components/practice2';
+import Input from './components/input';
+import Button from './components/button';
 
 function App() {
-	return (
-		<div>
-			<header className="bg-[#94A3B8] text-center p-[20px] text-4xl text-[#F9FAFB]">
-				React-v3
-			</header>
-			<Practice2 />
-		</div>
-	);
+  return (
+    <div>
+      <header className="bg-[#94A3B8] text-center p-5 text-4xl text-white">
+        React-v3
+      </header>
+      <div className="flex justify-center my-10">
+        <Input
+          type="text"
+          onChange={(e) => {
+            console.log(e.target.value);
+          }}
+          className="h-8 border-2"
+        />
+        <Button
+          buttonName="Reset"
+          backGroundColor="red"
+          onClick={() => {
+            console.log("Reset button clicked");
+          }}
+        />
+      </div>
+    </div>
+  );
 }
+
 export default App;
--- src/components/input.tsx
+++ src/components/input.tsx
@@ -1,3 +1,5 @@
+import React from 'react';
+
 interface InputProps {
   type: string;
   onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
