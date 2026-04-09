81300cd44cb5c131e91fe1a154cb6511f4f2f5dd
奥寺　将大 <masato_okudera@a7dsc014.local>
2026-04-09 15:21:55 +0900

    practice1＋前回の修正分

--- src/components/Practice1.tsx
+++ src/components/Practice1.tsx
@@ -3,15 +3,11 @@ import '../App.css';
 
 const Practice1 = () => {
   return (
-    <>
-      <div>
-        <div className="flex justify-center mt-[20px]">
-          <Button label="button1" />
-          <Button label="button2" />
-          <Button label="button3" />
-        </div>
-      </div>
-    </>
+    <div className="flex justify-center mt-[20px]">
+      <Button className="bg-[#f87171]" label="button1" />
+      <Button className="bg-[#fcd34d]" label="button2" />
+      <Button className="bg-[#86efac]" label="button3" />
+    </div>
   );
 };
 
