d3e88ef743137ea24f301736d2f0970cb1f913cb
奥寺　将大 <masato_okudera@a7dsc014.local>
2026-04-16 11:52:43 +0900

    不要コードの削除修正

--- src/components/elements/Input.tsx
+++ src/components/elements/Input.tsx
@@ -1,21 +1,4 @@
-import React, { ReactNode } from 'react';
-
-// interface Props {
-//   type: React.HTMLInputTypeAttribute;
-//   className: string;
-//   isChecked: boolean;
-//   onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
-//   children?: ReactNode;
-// }
-
-// const Input: React.FC<Props> = ({ type, className, isChecked, onChange, children }) => {
-//   return (
-//     <div className={className}>
-//       <input type={type} checked={isChecked} onChange={onChange} />
-//       {children}
-//     </div>
-//   );
-// };
+import React from 'react';
 
 interface Props {
   type: React.HTMLInputTypeAttribute;
