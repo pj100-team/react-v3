d86976ddd4127eeadd6654c683ba6c0e2e88d877
奥寺　将大 <masato_okudera@a7dsc014.local>
2026-05-07 10:33:26 +0900

    Practice5作成

---
+++ src/components/templates/Practice5.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Practice5() {
+  return (
+    <>
+      <div className="flex justify-center">TODOLIST</div>
+      <div>aaaaaaa</div>
+    </>
+  );
+}
+
+export default Practice5;
