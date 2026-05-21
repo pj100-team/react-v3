8deec91c2205f0802aaf1d8e6183d70287ff1de4
奥寺　将大 <masato_okudera@a7dsc014.local>
2026-05-21 15:23:02 +0900

    修正対応

--- src/components/templates/Home.tsx
+++ src/components/templates/Home.tsx
@@ -3,18 +3,14 @@ import { Link } from 'react-router-dom';
 
 const Home = () => {
   return (
-    <>
-      <div className="flex flex-col items-center mt-8">
-        <div className="flex flex-col items-center">
-          <Link to="/practice5" className="mb-4 text-lg">
-            TODOList
-          </Link>
-          <Link to="/practice4" className="mb-4 text-lg">
-            addressSearch
-          </Link>
-        </div>
-      </div>
-    </>
+    <div className="flex flex-col items-center mt-8">
+      <Link to="/practice5" className="mb-4 text-lg">
+        TODOList
+      </Link>
+      <Link to="/practice4" className="mb-4 text-lg">
+        addressSearch
+      </Link>
+    </div>
   );
 };
 
