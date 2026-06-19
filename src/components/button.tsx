58654aec5ef559966b4d43f287d6527f3b24b11b
勝矢　旭飛 <aasahi_katsuya@a7dsc054.local>
2026-06-19 16:31:03 +0900

    fix:[practice1] component & props

--- src/components/button.tsx
+++ src/components/button.tsx
@@ -1,21 +1,26 @@
-import { Practice1 } from "../templates/practice1";
-
-export const Button = () => {
-
-	return (
-		<div style={{ display: "flex", flexWrap: "wrap", justifyContent: "center", alignItems: "center", gap: "1rem", marginTop: "3rem" }}>
-			<Practice1
-			name="button1"
-			style={{ borderColor: "#f87171", backgroundColor: "#f87171", color: "#f9fafb", borderRadius: "0.375rem", padding: "0.75rem 3.5rem", fontSize: "1.125rem" }}
-			onClick={() => console.log("button1")} />
-			<Practice1
-			name="button2"
-			style={{ borderColor: "#fcd34d", backgroundColor: "#fcd34d", color: "#f9fafb", borderRadius: "0.375rem", padding: "0.75rem 3.5rem", fontSize: "1.125rem" }}
-			onClick={() => console.log("button2")} />
-			<Practice1
-			name="button3"
-			style={{ borderColor: "#86efac", backgroundColor: "#86efac", color: "#f9fafb", borderRadius: "0.375rem", padding: "0.75rem 3.5rem", fontSize: "1.125rem" }}
-			onClick={() => console.log("button3")} />
-		</div>
-	);
+interface ButtonProps {
+  label: string;
+  backgroundColor: string;
+  textColor: string;
+  onClick: () => void;
 }
+
+const Button: React.FC<ButtonProps> = ({ label, backgroundColor, textColor, onClick }) => {
+  return (
+    <button
+      style={{
+        borderColor: backgroundColor,
+        backgroundColor,
+        color: textColor,
+        borderRadius: "0.375rem",
+        padding: "0.75rem 3.5rem",
+        fontSize: "1.125rem",
+      }}
+      onClick={onClick}
+    >
+      {label}
+    </button>
+  );
+};
+
+export default Button;
