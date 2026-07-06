64b83a4c8283f57963f4103ec45305a79ba5ec51
社家　樹 <itsuki_shake@a7dsc037.local>
2026-07-06 12:09:05 +0900

    practice1の修正

--- src/components/elements/Button.tsx
+++ src/components/elements/Button.tsx
@@ -3,14 +3,12 @@ import React from 'react';
 interface ButtonProps {
   label: string;
   backgroundColor: string;
+  onClick: () => void;
 }
 
-const Button = ({ backgroundColor, label }: ButtonProps) => {
-  const handleClick = () => {
-    console.log(label);
-  };
+const Button = ({ backgroundColor, label, onClick }: ButtonProps) => {
   return (
-    <button className={`px-4 py-1 text-[#f9fafb] rounded-xl ${backgroundColor}`} onClick={handleClick}>
+    <button className={`px-4 py-1 text-[#f9fafb] rounded-xl ${backgroundColor}`} onClick={onClick}>
       {label}
     </button>
   );
